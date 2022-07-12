e7df8de8e51e53e858ded4e55c56023c492f2c68
Christopher Patton <cpatton@cloudflare.com>
2022-07-12 10:58:12 -0700

    Ensure ReportStore state is persistent
    Move all `ReportStore` memory to persistent storage via the storage API
    for durable objects. While at it, simplify the `ReportStore` durable
    object by handling reports as opaque hex strings.

---
+++ daphne_worker/src/durable/report_store_test.rs
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 Cloudflare, Inc. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+use crate::durable::report_store::nonce_hex_from_report;
+use daphne::messages::{Id, Nonce, Report};
+use prio::codec::{Decode, Encode};
+use rand::prelude::*;
+
+// Test that the `nonce_hex_from_report()` method properly extracts the nonce from the hex-encoded
+// report. This helps ensure that changes to the `Report` wire format don't cause any regressions
+// to `ReportStore`.
+#[test]
+fn test_nonce_hex_from_report() {
+    let mut rng = thread_rng();
+    let report = Report {
+        task_id: Id(rng.gen()),
+        nonce: Nonce {
+            time: rng.gen(),
+            rand: rng.gen(),
+        },
+        extensions: Vec::default(),
+        encrypted_input_shares: Vec::default(),
+    };
+
+    let report_hex = hex::encode(report.get_encoded());
+    let key = nonce_hex_from_report(&report_hex).unwrap();
+    assert_eq!(
+        Nonce::get_decoded(&hex::decode(key).unwrap()).unwrap(),
+        report.nonce
+    );
+}
