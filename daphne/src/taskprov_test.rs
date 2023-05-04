a04d95afc5e2f833c443ef2e95343c7c37741ef9
Christopher Patton <cpatton@cloudflare.com>
2023-05-04 16:02:53 -0700

    taskprov: Parse advertisement from `DapRequest.taskprov` if available

--- daphne/src/taskprov_test.rs
+++ daphne/src/taskprov_test.rs
@@ -1,11 +1,18 @@
 // Copyright (c) 2022 Cloudflare, Inc. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
+use prio::codec::ParameterizedEncode;
+
 use crate::{
-    messages::taskprov::VdafType,
-    messages::TaskId,
-    taskprov::{compute_vdaf_verify_key, TaskprovVersion},
+    auth::BearerToken,
+    hpke::HpkeReceiverConfig,
+    messages::{encode_base64url, taskprov::*, Extension, ReportId, ReportMetadata, TaskId},
+    messages::{taskprov::VdafType, HpkeKemId},
+    taskprov::{
+        compute_task_id, compute_vdaf_verify_key, resolve_advertised_task_config, TaskprovVersion,
+    },
     vdaf::VdafVerifyKey,
+    DapRequest,
 };
 
 #[test]
@@ -34,4 +41,111 @@ fn check_vdaf_key_computation() {
         VdafVerifyKey::Prio3(bytes) => assert_eq!(*bytes, expected),
         _ => unreachable!(),
     }
+}
+
+// Ensure that the task config is computed the same way whether it was advertised in the request
+// header or the report metadata.
+#[test]
+fn test_resolve_advertised_task_config() {
+    let taskprov_version = TaskprovVersion::Draft02;
+    let taskprov_task_config = TaskConfig {
+        task_info: "Hi".as_bytes().to_vec(),
+        aggregator_endpoints: vec![
+            UrlBytes {
+                bytes: "https://leader.com".as_bytes().to_vec(),
+            },
+            UrlBytes {
+                bytes: "https://helper.com".as_bytes().to_vec(),
+            },
+        ],
+        query_config: QueryConfig {
+            time_precision: 0x01,
+            max_batch_query_count: 128,
+            min_batch_size: 1024,
+            var: QueryConfigVar::FixedSize {
+                max_batch_size: 2048,
+            },
+        },
+        task_expiration: 0x6352f9a5,
+        vdaf_config: VdafConfig {
+            dp_config: DpConfig::None,
+            var: VdafTypeVar::Prio3Aes128Histogram {
+                buckets: vec![1, 2, 3],
+            },
+        },
+    };
+
+    let taskprov_task_config_data = taskprov_task_config.get_encoded_with_param(&taskprov_version);
+    let taskprov_task_config_base64url = encode_base64url(&taskprov_task_config_data);
+    let task_id = compute_task_id(taskprov_version, &taskprov_task_config_data).unwrap();
+    let collector_hpke_config = HpkeReceiverConfig::gen(1, HpkeKemId::X25519HkdfSha256)
+        .unwrap()
+        .config;
+
+    let from_request_header = resolve_advertised_task_config(
+        &DapRequest::<BearerToken> {
+            task_id: Some(task_id.clone()),
+            taskprov: Some(taskprov_task_config_base64url),
+            ..Default::default()
+        },
+        taskprov_version,
+        &[0; 32],
+        &collector_hpke_config,
+        &task_id,
+        None,
+    )
+    .unwrap()
+    .unwrap();
+
+    let from_report_metadata = resolve_advertised_task_config(
+        &DapRequest::<BearerToken> {
+            task_id: Some(task_id.clone()),
+            ..Default::default()
+        },
+        taskprov_version,
+        &[0; 32],
+        &collector_hpke_config,
+        &task_id,
+        Some(&ReportMetadata {
+            id: ReportId([0; 16]),
+            time: 0,
+            extensions: vec![Extension::Taskprov {
+                payload: taskprov_task_config_data,
+            }],
+        }),
+    )
+    .unwrap()
+    .unwrap();
+
+    assert_eq!(from_request_header.version, from_report_metadata.version);
+    assert_eq!(
+        from_request_header.leader_url,
+        from_report_metadata.leader_url
+    );
+    assert_eq!(
+        from_request_header.helper_url,
+        from_report_metadata.helper_url
+    );
+    assert_eq!(
+        from_request_header.time_precision,
+        from_report_metadata.time_precision
+    );
+    assert_eq!(
+        from_request_header.expiration,
+        from_report_metadata.expiration
+    );
+    assert_eq!(
+        from_request_header.min_batch_size,
+        from_report_metadata.min_batch_size
+    );
+    assert_eq!(from_request_header.query, from_report_metadata.query);
+    assert_eq!(from_request_header.vdaf, from_report_metadata.vdaf);
+    assert_eq!(
+        from_request_header.vdaf_verify_key.as_ref(),
+        from_report_metadata.vdaf_verify_key.as_ref()
+    );
+    assert_eq!(
+        from_request_header.collector_hpke_config,
+        from_report_metadata.collector_hpke_config
+    );
 }
