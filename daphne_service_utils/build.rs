548e1d336239699075d0ef4696efd4895c87d912
pmendes <pmendes@cloudflare.com>
2024-04-05 14:46:13 +0100

    Feature gate daphne_service_utils dependency on capnproto
    This lets dapf not depend on capnproto which helps keep CI builds
    simpler.

--- daphne_service_utils/build.rs
+++ daphne_service_utils/build.rs
@@ -1,4 +1,8 @@
+// Copyright (c) 2024 Cloudflare, Inc. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
 fn main() {
+    #[cfg(feature = "durable_requests")]
     ::capnpc::CompilerCommand::new()
         .file("./src/durable_requests/durable_request.capnp")
         .run()
