4f2f843e48cd9ca3c630b6ccff3d3585bb8d3e48
Christopher Patton <cpatton@cloudflare.com>
2024-08-27 15:21:21 -0700

    pine: Add additional test vectors and fix a bug
    Add additional test vectors for PINE. While at it, make the test runner
    a one-liner that can be more easily copy+pasted.
    
    The new vectors revealed a bug in the `eval_wr_checks()` sub-circuit
    related to improper handling of the last chunk of inputs to the gadget.
    
    The bug is an incorrect order of operations that was triggered by the
    number of wraparound tests used by the new test vectors. The last chunk
    is skipped if the condition `buf.len() % self.cfg.param.chunk_len * 2 !=
    0` holds, where `buf` is the last chunk. In our case, `buf.len()` is 150
    (2 times 75 wraparound tests) and `self.cfg.param.chunk_len` is 150. The
    expression gets evaluated as `(150 % 150) * 2 != 0`, not `150 % (150 *
    2) != 0`, and thus the last chunk gets skipped.
    
    The correct condition is `!buf.is_empty()`.

--- crates/daphne/src/pine/test_vec/mod.rs
+++ crates/daphne/src/pine/test_vec/mod.rs
@@ -2,14 +2,17 @@
 
 //! Tools for evaluating PINE test vectors.
 
+use crate::pine::{Pine, PineParam, ALPHA};
 use prio::{
     codec::Encode,
     field::FftFriendlyFieldElement,
-    vdaf::{xof::XofTurboShake128, Aggregator, Collector, PrepareTransition},
+    field::{Field128, Field64},
+    vdaf::{
+        xof::{Xof, XofHmacSha256Aes128, XofTurboShake128},
+        Aggregator, Collector, PrepareTransition,
+    },
 };
 use serde::Deserialize;
-
-use crate::pine::{Pine, ALPHA};
 
 #[derive(Deserialize)]
 struct Encoded(#[serde(with = "hex")] Vec<u8>);
@@ -44,7 +47,7 @@ struct TestVec {
     chunk_length: usize,
     chunk_length_norm_equality: usize,
     shares: usize,
-    proofs: usize,
+    proofs: u8,
 
     // Execution
     verify_key: Encoded,
@@ -54,35 +57,51 @@ struct TestVec {
     agg_result: Vec<f64>,
 }
 
-impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
-    fn run_test_vec(&self, test_vec: &TestVec) {
+impl TestVec {
+    fn from_str(test_vec_str: &str) -> Self {
+        serde_json::from_str(test_vec_str).unwrap()
+    }
+
+    fn run<F: FftFriendlyFieldElement, X: Xof<SEED_SIZE>, const SEED_SIZE: usize>(&self) {
+        let pine = Pine::<F, X, SEED_SIZE>::new(
+            &PineParam {
+                norm_bound: self.l2_norm_bound,
+                dimension: self.dimension,
+                frac_bits: self.num_frac_bits,
+                chunk_len: self.chunk_length,
+                chunk_len_sq_norm_equal: self.chunk_length_norm_equality,
+                num_proofs: self.proofs,
+                num_wr_tests: self.num_wr_checks,
+                num_wr_successes: self.num_wr_successes,
+            },
+            0xffff_ffff,
+        )
+        .unwrap();
+
         // Check that the test vector parameters have the values we expect.
         //
         // clippy: These are test vectors, so we expect the value to match precisely.
         #[allow(clippy::float_cmp)]
         {
-            assert_eq!(test_vec.alpha, ALPHA);
+            assert_eq!(self.alpha, ALPHA);
         }
-        assert_eq!(test_vec.num_wr_checks, 100);
-        assert_eq!(test_vec.num_wr_successes, 100);
-        assert_eq!(test_vec.shares, 2);
-        assert_eq!(test_vec.proofs, usize::from(self.flp.cfg.param.num_proofs));
+        assert_eq!(self.shares, 2);
 
         let mut out_shares_0 = Vec::new();
         let mut out_shares_1 = Vec::new();
-        let verify_key = <[u8; 16]>::try_from(test_vec.verify_key.as_ref()).unwrap();
-        for report in &test_vec.reports {
+        let verify_key = <[u8; SEED_SIZE]>::try_from(self.verify_key.as_ref()).unwrap();
+        for report in &self.reports {
             let rand = report
                 .rand
                 .as_ref()
-                .chunks(16)
-                .map(|seed| <[u8; 16]>::try_from(seed).unwrap())
+                .chunks(SEED_SIZE)
+                .map(|seed| <[u8; SEED_SIZE]>::try_from(seed).unwrap())
                 .take(7)
                 .collect::<Vec<_>>();
             let nonce = <[u8; 16]>::try_from(report.nonce.as_ref()).unwrap();
 
             // Sharding
-            let (public_share, input_shares) = self
+            let (public_share, input_shares) = pine
                 .shard_with_rand(&report.measurement, &nonce, rand.try_into().unwrap())
                 .unwrap();
             assert_eq!(
@@ -102,7 +121,7 @@ impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
             );
 
             // Preparation
-            let (prep_state_0, prep_share_0) = self
+            let (prep_state_0, prep_share_0) = pine
                 .prepare_init(&verify_key, 0, &(), &nonce, &public_share, &input_shares[0])
                 .unwrap();
             assert_eq!(
@@ -111,7 +130,7 @@ impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
                 "prep share 0",
             );
 
-            let (prep_state_1, prep_share_1) = self
+            let (prep_state_1, prep_share_1) = pine
                 .prepare_init(&verify_key, 1, &(), &nonce, &public_share, &input_shares[1])
                 .unwrap();
             assert_eq!(
@@ -120,7 +139,7 @@ impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
                 "prep share 1",
             );
 
-            let prep = self
+            let prep = pine
                 .prepare_shares_to_prepare_message(&(), [prep_share_0, prep_share_1])
                 .unwrap();
             assert_eq!(
@@ -130,7 +149,7 @@ impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
             );
 
             let PrepareTransition::Finish(out_share_0) =
-                self.prepare_next(prep_state_0, prep.clone()).unwrap()
+                pine.prepare_next(prep_state_0, prep.clone()).unwrap()
             else {
                 panic!("unexpected transition");
             };
@@ -141,7 +160,7 @@ impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
             out_shares_0.push(out_share_0);
 
             let PrepareTransition::Finish(out_share_1) =
-                self.prepare_next(prep_state_1, prep.clone()).unwrap()
+                pine.prepare_next(prep_state_1, prep.clone()).unwrap()
             else {
                 panic!("unexpected transition");
             };
@@ -153,57 +172,66 @@ impl<F: FftFriendlyFieldElement> Pine<F, XofTurboShake128, 16> {
         }
 
         // Aggregation
-        let agg_share_0 = self.aggregate(&(), out_shares_0).unwrap();
+        let agg_share_0 = pine.aggregate(&(), out_shares_0).unwrap();
         assert_eq!(
             agg_share_0.get_encoded().unwrap(),
-            test_vec.agg_shares[0].as_ref()
+            self.agg_shares[0].as_ref()
         );
 
-        let agg_share_1 = self.aggregate(&(), out_shares_1).unwrap();
+        let agg_share_1 = pine.aggregate(&(), out_shares_1).unwrap();
         assert_eq!(
             agg_share_1.get_encoded().unwrap(),
-            test_vec.agg_shares[1].as_ref()
+            self.agg_shares[1].as_ref()
         );
 
         // Unsharding
-        let agg_result = self
-            .unshard(&(), [agg_share_0, agg_share_1], test_vec.reports.len())
+        let agg_result = pine
+            .unshard(&(), [agg_share_0, agg_share_1], self.reports.len())
             .unwrap();
-        assert_eq!(agg_result, test_vec.agg_result);
+        assert_eq!(agg_result, self.agg_result);
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn pine64_0() {
+    TestVec::from_str(include_str!("01/Pine64_0.json")).run::<Field64, XofTurboShake128, 16>();
+}
 
-    use crate::pine::Pine;
+#[test]
+fn pine64_1() {
+    TestVec::from_str(include_str!("01/Pine64_1.json")).run::<Field64, XofTurboShake128, 16>();
+}
 
-    #[test]
-    fn run_64() {
-        let test_vec = serde_json::from_str::<TestVec>(include_str!("01/Pine64_0.json")).unwrap();
-        Pine::new_64(
-            test_vec.l2_norm_bound,
-            test_vec.dimension,
-            test_vec.num_frac_bits,
-            test_vec.chunk_length,
-            test_vec.chunk_length_norm_equality,
-        )
-        .unwrap()
-        .run_test_vec(&test_vec);
-    }
+#[test]
+fn pine128_0() {
+    TestVec::from_str(include_str!("01/Pine128_0.json")).run::<Field128, XofTurboShake128, 16>();
+}
 
-    #[test]
-    fn run_128() {
-        let test_vec = serde_json::from_str::<TestVec>(include_str!("01/Pine128_0.json")).unwrap();
-        Pine::new_128(
-            test_vec.l2_norm_bound,
-            test_vec.dimension,
-            test_vec.num_frac_bits,
-            test_vec.chunk_length,
-            test_vec.chunk_length_norm_equality,
-        )
-        .unwrap()
-        .run_test_vec(&test_vec);
-    }
+#[test]
+fn pine128_1() {
+    TestVec::from_str(include_str!("01/Pine128_1.json")).run::<Field128, XofTurboShake128, 16>();
+}
+
+#[test]
+fn pine64_hmac_sha256_aes128_0() {
+    TestVec::from_str(include_str!("01/Pine64XofHmacSha256Aes128_0.json"))
+        .run::<Field64, XofHmacSha256Aes128, 32>();
+}
+
+#[test]
+fn pine64_hmac_sha256_aes128_1() {
+    TestVec::from_str(include_str!("01/Pine64XofHmacSha256Aes128_1.json"))
+        .run::<Field64, XofHmacSha256Aes128, 32>();
+}
+
+#[test]
+fn pine128_hmac_sha256_aes128_0() {
+    TestVec::from_str(include_str!("01/Pine128XofHmacSha256Aes128_0.json"))
+        .run::<Field128, XofHmacSha256Aes128, 32>();
+}
+
+#[test]
+fn pine128_hmac_sha256_aes128_1() {
+    TestVec::from_str(include_str!("01/Pine128XofHmacSha256Aes128_1.json"))
+        .run::<Field128, XofHmacSha256Aes128, 32>();
 }
